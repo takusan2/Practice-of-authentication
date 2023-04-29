64ec0090b5cfa4a578b6fdd839d8ef1b16b28c43
hapitaku <106806920+hapitaku@users.noreply.github.com>
2023-04-30 00:48:57 +0900

    modify entity.User

--- repository/entity/user.go
+++ repository/entity/user.go
@@ -8,11 +8,10 @@ import (
 
 type User struct {
 	bun.BaseModel `bun:"table:users,alias:u"`
-
-	ID           int64     `bun:"id,pk,autoincrement"`
-	Name         string    `bun:"name,notnull"`
-	Email        string    `bun:"email,notnull,unique"`
-	PasswordHash string    `bun:"password_hash,notnull"`
-	CreatedAt    time.Time `bun:"created_at,notnull,default:current_timestamp"`
-	UpdatedAt    time.Time `bun:"updated_at,notnull,default:current_timestamp"`
+	ID            int64     `bun:"id,pk,autoincrement"`
+	Name          string    `bun:"name,notnull"`
+	Email         string    `bun:"email,notnull,unique"`
+	PasswordHash  string    `bun:"password_hash,notnull"`
+	CreatedAt     time.Time `bun:"created_at,notnull,default:current_timestamp"`
+	UpdatedAt     time.Time `bun:"updated_at,notnull,default:current_timestamp"`
 }
