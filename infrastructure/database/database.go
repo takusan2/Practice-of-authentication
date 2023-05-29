dbe9de47a01bcf255df83f103c8ba66f736c88c2
hapitaku <106806920+hapitaku@users.noreply.github.com>
2023-05-29 14:51:43 +0900

    dsnの設定

--- infrastructure/database/database.go
+++ infrastructure/database/database.go
@@ -5,11 +5,12 @@ import (
 	"os"
 
 	"github.com/jinzhu/gorm"
+	"github.com/joho/godotenv"
 	"github.com/takuya-okada-01/heart-note/domain"
+	_ "gorm.io/driver/mysql"
 )
 
 func Connect() *gorm.DB {
-
 	// cfg := sql.Config{
 	// 	User:                 os.Getenv("DBUSER"),
 	// 	Passwd:               os.Getenv("DBPASS"),
@@ -20,7 +21,10 @@ func Connect() *gorm.DB {
 	// 	AllowNativePasswords: true,
 	// }
 	// dsn := cfg.FormatDSN()
+	godotenv.Load("/Users/okadatakuya/my_folder/dev/my_app/（仮）/backend/.env")
+
 	dsn := os.Getenv("CLEARDB_DATABASE_URL")
+	fmt.Print("dsn", dsn)
 	db, err := gorm.Open("mysql", dsn)
 	db.LogMode(true)
 	if err != nil {
