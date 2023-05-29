1ffd677bed321de5117af3e0334e259d0ca21992
hapitaku <106806920+hapitaku@users.noreply.github.com>
2023-05-29 15:15:43 +0900

    dsn

--- infrastructure/database/database.go
+++ infrastructure/database/database.go
@@ -4,27 +4,25 @@ import (
 	"fmt"
 	"os"
 
+	sql "github.com/go-sql-driver/mysql"
 	"github.com/jinzhu/gorm"
-	"github.com/joho/godotenv"
 	"github.com/takuya-okada-01/heart-note/domain"
-	_ "gorm.io/driver/mysql"
 )
 
 func Connect() *gorm.DB {
-	// cfg := sql.Config{
-	// 	User:                 os.Getenv("DBUSER"),
-	// 	Passwd:               os.Getenv("DBPASS"),
-	// 	Net:                  os.Getenv("NET"),
-	// 	Addr:                 os.Getenv("ADDR"),
-	// 	DBName:               os.Getenv("DBNAME"),
-	// 	ParseTime:            true,
-	// 	AllowNativePasswords: true,
-	// }
-	// dsn := cfg.FormatDSN()
-	godotenv.Load("/Users/okadatakuya/my_folder/dev/my_app/（仮）/backend/.env")
+	// godotenv.Load("/Users/okadatakuya/my_folder/dev/my_app/（仮）/backend/.env")
 
-	dsn := os.Getenv("CLEARDB_DATABASE_URL")
-	fmt.Print("dsn", dsn)
+	cfg := sql.Config{
+		User:                 os.Getenv("DBUSER"),
+		Passwd:               os.Getenv("DBPASS"),
+		Net:                  os.Getenv("NET"),
+		Addr:                 os.Getenv("ADDR"),
+		DBName:               os.Getenv("DBNAME"),
+		ParseTime:            true,
+		AllowNativePasswords: true,
+	}
+	dsn := cfg.FormatDSN()
+
 	db, err := gorm.Open("mysql", dsn)
 	db.LogMode(true)
 	if err != nil {
