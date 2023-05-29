b1b75a22aea6085adc862bcb16c14eaaf18e8553
hapitaku <106806920+hapitaku@users.noreply.github.com>
2023-05-29 14:31:58 +0900

    dsn 設定

--- infrastructure/database/database.go
+++ infrastructure/database/database.go
@@ -4,24 +4,24 @@ import (
 	"fmt"
 	"os"
 
-	sql "github.com/go-sql-driver/mysql"
 	"github.com/jinzhu/gorm"
 	"github.com/takuya-okada-01/heart-note/domain"
 )
 
 func Connect() *gorm.DB {
 
-	cfg := sql.Config{
-		User:                 os.Getenv("DBUSER"),
-		Passwd:               os.Getenv("DBPASS"),
-		Net:                  os.Getenv("NET"),
-		Addr:                 os.Getenv("ADDR"),
-		DBName:               os.Getenv("DBNAME"),
-		ParseTime:            true,
-		AllowNativePasswords: true,
-	}
-
-	db, err := gorm.Open("mysql", cfg.FormatDSN())
+	// cfg := sql.Config{
+	// 	User:                 os.Getenv("DBUSER"),
+	// 	Passwd:               os.Getenv("DBPASS"),
+	// 	Net:                  os.Getenv("NET"),
+	// 	Addr:                 os.Getenv("ADDR"),
+	// 	DBName:               os.Getenv("DBNAME"),
+	// 	ParseTime:            true,
+	// 	AllowNativePasswords: true,
+	// }
+	// dsn := cfg.FormatDSN()
+	dsn := os.Getenv("CLEARDB_DATABASE_URL")
+	db, err := gorm.Open("mysql", dsn)
 	db.LogMode(true)
 	if err != nil {
 		panic(err.Error())
